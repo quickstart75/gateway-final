794c181f430073e41ab3a0f4fa6ad7f2742cb27f
muhammadrehan-360training <muhammad.rehan@360training.com>
2016-12-20 12:49:20 +0500

    INTLSKILLS-190 - course outline info Service

--- LS360ApiGatewayService/src/test/java/com/softech/ls360/api/gateway/service/LearnerProfileEndPointServiceTest.java
+++ LS360ApiGatewayService/src/test/java/com/softech/ls360/api/gateway/service/LearnerProfileEndPointServiceTest.java
@@ -1,11 +1,21 @@
 package com.softech.ls360.api.gateway.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.junit.Test;
 
+import com.softech.ls360.api.gateway.service.model.request.CourseRequest;
+
 public class LearnerProfileEndPointServiceTest extends LS360ApiGatewayServiceAbstractTest {
 
+	@Inject
+	CourseService courseService;
+	
 	@Inject
 	private LearnerProfileEndPointService learnerProfileEndPointService;;
 	
@@ -14,11 +24,29 @@ public class LearnerProfileEndPointServiceTest extends LS360ApiGatewayServiceAbs
 		
 	}
 	
-	@Test
+	//@Test
 	public void getLearnerProfileByUserName() {
 		String userName = "SQA_demo_customer@360training.com";
 		try {
 			learnerProfileEndPointService.getLearnerProfile(userName);;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void getCourseOutlineByGuids() {
+		CourseRequest courseRequest = new CourseRequest();
+		List<String> courseGuids = new ArrayList<String>();
+		
+		courseGuids.add("14F93637DED84498BCBB8A336A1A18611");
+		courseGuids.add("a75026b92ce44e7fad47e321b56654211");
+		
+		try {
+			Map<String, String> lstCourseOutlines = new HashMap<String, String>();
+			lstCourseOutlines = courseService.getCourseOutlineByGuids(courseGuids);;
+			System.out.println(lstCourseOutlines.size());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
