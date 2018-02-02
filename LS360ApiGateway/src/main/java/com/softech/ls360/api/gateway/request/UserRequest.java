b96955cee24157bf39e2a8b2848eb42d2b74986d
muhammadrehan-360training <Rehan.Rana@quickstart.com>
2018-02-02 21:53:04 +0500

    INTLSKILLS-2645 - Manager can see User Analytics

---
+++ LS360ApiGateway/src/main/java/com/softech/ls360/api/gateway/request/UserRequest.java
@@ -0,0 +1,14 @@
+package com.softech.ls360.api.gateway.request;
+
+public class UserRequest {
+	String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+}
