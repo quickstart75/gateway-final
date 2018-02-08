857878d60827fbe45265092bd8a1da0589699862
Muhammad.Rehan <Muhammad.Rehan@QUICKSTART-027.quickstart.local>
2018-02-08 20:59:20 +0500

    [INTLSKILLS-2637]-> Detailed Report on 4 Key STATS for a Manager

--- LmsRepository/src/main/java/com/softech/ls360/lms/repository/projection/VU360UserProjection.java
+++ LmsRepository/src/main/java/com/softech/ls360/lms/repository/projection/VU360UserProjection.java
@@ -1,15 +1,20 @@
 package com.softech.ls360.lms.repository.projection;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+
 public interface VU360UserProjection {
 	Long getId();
 	String getFirstname();
 	String getLastname();
 	String getEmail();
 	String getUsername();
+	
+	//@Value("#{VU360User.lastLogOnDate}")
+	@Value("#{args[5]}")
+	String getLastLogOnDate();
 	Long getLearnergroupid();
 	String getLearnergroupname();
 	
-	
-	
-	//select vu.id as id, vu.firstName as firstName, vu.lastName as lastName, vu.emailAddress as email, vu.username as username, lg.id as learnerGroupId, lg.name as learnerGroupName"
 }
