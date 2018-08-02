93574424870e73a0afe4498403244e03cd1b8eb3
muhammadrehan-360training <muhammad.rehan@360training.com>
2018-08-02 16:24:52 +0500

    MagantoCategoryRepository and MagantoCategor entity

---
+++ LmsRepository/src/main/java/com/softech/ls360/lms/repository/repositories/MagantoCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.softech.ls360.lms.repository.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.softech.ls360.lms.repository.entities.MagantoCategory;
+
+public interface MagantoCategoryRepository extends CrudRepository<MagantoCategory, Long>  {
+
+}
