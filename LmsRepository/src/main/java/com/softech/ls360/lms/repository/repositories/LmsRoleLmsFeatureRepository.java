21fe33095c672ad603edcc0e1a71d4872e548383
muhammadrehan-360training <muhammad.rehan@360training.com>
2019-06-10 21:22:24 +0500

    VPA getRoleTypesByUsername

--- LmsRepository/src/main/java/com/softech/ls360/lms/repository/repositories/LmsRoleLmsFeatureRepository.java
+++ LmsRepository/src/main/java/com/softech/ls360/lms/repository/repositories/LmsRoleLmsFeatureRepository.java
@@ -3,7 +3,9 @@ package com.softech.ls360.lms.repository.repositories;
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.softech.ls360.lms.repository.entities.LmsRoleLmsFeature;
 
@@ -11,4 +13,11 @@ public interface LmsRoleLmsFeatureRepository extends CrudRepository<LmsRoleLmsFe
 
 	List<LmsRoleLmsFeature> findByLmsRoleIdIn(Collection<Long> ids);
 	LmsRoleLmsFeature findByLmsRoleIdAndLmsFeature_FeatureName(Long id, String featureName);
+	
+	@Query(value =  " SELECT R.Role_Type FROM VU360USER U " +
+			" INNER JOIN VU360USER_ROLE UR ON U.ID = UR.USER_ID  " +
+			" INNER JOIN LMSROLE R ON R.ID = UR.ROLE_ID " +
+			" WHERE U.USERNAME=:usrename ", nativeQuery = true)
+	List<String> getRoleTypesByUsername(@Param("usrename") String usrename);
+
 }
