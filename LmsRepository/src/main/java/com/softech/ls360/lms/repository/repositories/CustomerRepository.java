10f1be6fdc3f385f39e5ac41cde286069ce5eb68
muhammadrehan-360training <muhammad.rehan@360training.com>
2019-07-09 22:07:35 +0500

    VPA utilization report

--- LmsRepository/src/main/java/com/softech/ls360/lms/repository/repositories/CustomerRepository.java
+++ LmsRepository/src/main/java/com/softech/ls360/lms/repository/repositories/CustomerRepository.java
@@ -82,7 +82,12 @@ public interface CustomerRepository extends CrudRepository<Customer, Long> {
    	void updateEnrollmentAndOrderStatus(@Param("enrollmentstatus") String enrollmentstatus, @Param("orderstatus") String orderstatus, @Param("id") Long id);
 
 	
-	@Query(value = " select ce.id,  oi.orderguid, (ce.SEATS - ce.NUMBERSEATSUSED) as remainingSeats , u.firstname, u.lastname, u.emailaddress, sk.name as subscriptionName, oi.OrderDate " +
+	
+	
+	
+	
+	
+	@Query(value = " select ce.id,  oi.orderguid, (ce.SEATS - ce.NUMBERSEATSUSED) as remainingSeats , u.firstname, u.lastname, u.emailaddress, sk.name as subscriptionName, oi.OrderDate, sk.guid " +
 			" FROM CUSTOMERENTITLEMENT ce   " +
 			" inner join OrderLineItem oli on oli.ENTITLEMENT_ID = ce.id  " +
 			" inner join orderinfo oi on oi.id = oli.ORDER_ID and ce.CUSTOMER_ID = oi.Customer_ID " +
@@ -94,7 +99,7 @@ public interface CustomerRepository extends CrudRepository<Customer, Long> {
 	List<Object[]> getVPAOrdersByCustomer(@Param("customerId") Long customerId, @Param("paymentMethod") String paymentMethod);
 	
 	
-	@Query(value = " SELECT  ce.id,  oi.orderguid, (ce.SEATS - ce.NUMBERSEATSUSED) as remainingSeats, sk.name,  oi.OrderDate   " +
+	@Query(value = " SELECT  ce.id,  oi.orderguid, (ce.SEATS - ce.NUMBERSEATSUSED) as remainingSeats, sk.name,  oi.OrderDate, sk.guid   " +
 			" FROM CUSTOMERENTITLEMENT ce   " +
 			" inner join OrderLineItem oli on oli.ENTITLEMENT_ID = ce.id   " +
 			" inner join orderinfo oi on oi.id = oli.ORDER_ID and ce.CUSTOMER_ID = oi.Customer_ID " +
@@ -103,8 +108,8 @@ public interface CustomerRepository extends CrudRepository<Customer, Long> {
 			" where ce.customer_id = :customerId and paymentMethod=:paymentMethod ", nativeQuery = true)
 	List<Object[]> getVPAOrdersByCustomerForSubscriptionCount(@Param("customerId") Long customerId, @Param("paymentMethod") String paymentMethod);
 
-	
-	@Query(value = " SELECT ce.id as cusId, u.firstname, u.lastname, u.emailaddress, c.name as courseName, oi.OrderDate, oi.orderguid  " +
+	// note: select columns will be same as of function getVPAOrdersByCustomerForCourseByTeam()
+	@Query(value = " SELECT ce.id as cusId, u.firstname, u.lastname, u.emailaddress, c.name as courseName, oi.OrderDate, oi.orderguid, c.guid  , SC.CLASSSTARTDATE, SC.CLASSENDDATE  " +
 			" FROM vu360user u   " +
 			" inner join Learner l on l.vu360user_id=u.id   " +
 			" inner join customer cu on cu.id = l.CUSTOMER_ID " +
@@ -113,11 +118,12 @@ public interface CustomerRepository extends CrudRepository<Customer, Long> {
 			" inner join CUSTOMERENTITLEMENT ce on ce.id=le.CUSTOMERENTITLEMENT_id and ce.customer_id = cu.ID " +
 			" inner join OrderLineItem oli on oli.ENTITLEMENT_ID = ce.id  " +
 			" inner join orderinfo oi on oi.id = oli.ORDER_ID and ce.CUSTOMER_ID = oi.Customer_ID " +
+			" LEFT JOIN SYNCHRONOUSCLASS SC ON SC.ID = LE.SYNCHRONOUSCLASS_ID " + 
 			" where ce.customer_id=:customerId  and paymentMethod=:paymentMethod ", nativeQuery = true)
 	List<Object[]> getVPAOrdersByCustomerForCourse(@Param("customerId") Long customerId, @Param("paymentMethod") String paymentMethod);
 	
 	
-	@Query(value = " SELECT ce.id,  oi.orderguid, (ce.SEATS - ce.NUMBERSEATSUSED) as remainingSeats, c.name, oi.OrderDate   " +
+	@Query(value = " SELECT ce.id,  oi.orderguid, (ce.SEATS - ce.NUMBERSEATSUSED) as remainingSeats, c.name, oi.OrderDate, c.guid   " +
 			" FROM CUSTOMERENTITLEMENT ce    " +
 			" inner join course_CUSTOMERENTITLEMENT cce on ce.id=cce.CUSTOMERENTITLEMENT_id  " +
 			"  inner join course c on c.id = cce.course_id " +
@@ -125,4 +131,39 @@ public interface CustomerRepository extends CrudRepository<Customer, Long> {
 			"  inner join orderinfo oi on oi.id = oli.ORDER_ID and ce.CUSTOMER_ID = oi.Customer_ID  " +
 			"  where ce.customer_id=:customerId  and paymentMethod=:paymentMethod " , nativeQuery = true)
 	List<Object[]> getVPAOrdersByCustomerForCourseCount(@Param("customerId") Long customerId, @Param("paymentMethod") String paymentMethod);
+	
+	
+	
+	
+	
+	
+	@Query(value = " select ce.id,  oi.orderguid, (ce.SEATS - ce.NUMBERSEATSUSED) as remainingSeats , u.firstname, u.lastname, u.emailaddress, sk.name as subscriptionName, oi.OrderDate, sk.guid, lg.id as teamId " +
+			" FROM CUSTOMERENTITLEMENT ce   " +
+			" inner join OrderLineItem oli on oli.ENTITLEMENT_ID = ce.id  " +
+			" inner join orderinfo oi on oi.id = oli.ORDER_ID and ce.CUSTOMER_ID = oi.Customer_ID " +
+			" Inner join subscription s on ce.id=s.CUSTOMERENTITLEMENT_id " +
+			" inner join subscription_kit sk on sk.id = s.subscription_kit_id " +
+			" inner join SUBSCRIPTION_USER su on su.subscription_id = s.id  " +
+			" inner join vu360user u on u.id = su.VU360USER_ID " +
+			" inner join learner l on l.vu360user_id = u.id " + 
+			" inner join LEARNER_LEARNERGROUP lgm on  lgm.learner_id=l.id  " + 
+			" inner join learnergroup lg on lg.customer_id = l.customer_id and lgm.learnergroup_id = lg.id " + 
+			" where ce.customer_id = :customerId    and   paymentMethod=:paymentMethod and  lg.id = :teamId ", nativeQuery = true)
+	List<Object[]> getVPAOrdersByCustomerByTeam(@Param("customerId") Long customerId, @Param("paymentMethod") String paymentMethod,  @Param("teamId") String teamId);
+	
+	// note: select columns will be same as of function getVPAOrdersByCustomerForCourse()
+	@Query(value = " SELECT ce.id as cusId, u.firstname, u.lastname, u.emailaddress, c.name as courseName, oi.OrderDate, oi.orderguid, c.guid ,  SC.CLASSSTARTDATE, SC.CLASSENDDATE " +
+			" FROM vu360user u   " +
+			" inner join Learner l on l.vu360user_id=u.id   " +
+			" inner join customer cu on cu.id = l.CUSTOMER_ID " +
+			" inner join LEARNERENROLLMENT le on le.LEARNER_ID=l.id  " +
+			" inner join course c on c.id=le.course_id  " +
+			" inner join CUSTOMERENTITLEMENT ce on ce.id=le.CUSTOMERENTITLEMENT_id and ce.customer_id = cu.ID " +
+			" inner join OrderLineItem oli on oli.ENTITLEMENT_ID = ce.id  " +
+			" inner join orderinfo oi on oi.id = oli.ORDER_ID and ce.CUSTOMER_ID = oi.Customer_ID " +
+			" inner join LEARNER_LEARNERGROUP lgm on  lgm.learner_id=l.id  " +
+			" inner join learnergroup lg on lg.customer_id = l.customer_id and lgm.learnergroup_id = lg.id " +
+			" LEFT JOIN SYNCHRONOUSCLASS SC ON SC.ID = LE.SYNCHRONOUSCLASS_ID " +
+			" where ce.customer_id=:customerId  and paymentMethod=:paymentMethod and  lg.id = :teamId", nativeQuery = true)
+	List<Object[]> getVPAOrdersByCustomerForCourseByTeam(@Param("customerId") Long customerId, @Param("paymentMethod") String paymentMethod,  @Param("teamId") String teamId);
 }
