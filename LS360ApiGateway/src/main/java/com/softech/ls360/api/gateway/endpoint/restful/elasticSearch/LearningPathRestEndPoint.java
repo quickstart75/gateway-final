1d57bcb842cd5e1580ca00bf5527351e42cffb2e
Zain.Noushad <Zain.Noushad@QUICKSTART-131>
2019-08-06 14:04:21 +0500

    Learner-Detail API improvement

--- LS360ApiGateway/src/main/java/com/softech/ls360/api/gateway/endpoint/restful/elasticSearch/LearningPathRestEndPoint.java
+++ LS360ApiGateway/src/main/java/com/softech/ls360/api/gateway/endpoint/restful/elasticSearch/LearningPathRestEndPoint.java
@@ -163,7 +163,7 @@ public class LearningPathRestEndPoint {
 			case "1": return "Beginner";
 			case "2": return "Intermediate";
 			case "3": return "Advanced";
-			default: return "0";	//No difficulty
+			default: return "1";	//No difficulty
 		}
 	}
 	
