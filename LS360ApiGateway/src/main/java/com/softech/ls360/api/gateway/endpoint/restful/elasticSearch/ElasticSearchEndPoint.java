1100863593ef7872e94054c1f6b29a90e3fb667b
Zain.Noushad <Zain.Noushad@QUICKSTART-131>
2019-09-13 16:34:54 +0500

    Condition update in ElasticSearch & Bug fix in Personalized API

--- LS360ApiGateway/src/main/java/com/softech/ls360/api/gateway/endpoint/restful/elasticSearch/ElasticSearchEndPoint.java
+++ LS360ApiGateway/src/main/java/com/softech/ls360/api/gateway/endpoint/restful/elasticSearch/ElasticSearchEndPoint.java
@@ -407,7 +407,7 @@ public class ElasticSearchEndPoint {
 			//---------------------------------------------------------------------------------------------------
 			if(filterEnrolledOrSubscription.equals("all") && (request.getUuid()!=null && !request.getUuid().equals(""))){
 				//If no Subsbcode 
-				if((onjESearch.getSubscriptions() ==null || onjESearch.getSubscriptions().size()==0)) {
+				if((onjESearch.getSubscriptions() !=null || onjESearch.getSubscriptions().size()>0)) {
 					List<String> mocLearningPaths = this.getGraphQLData(request.getUuid());
 					onjESearch.setSubsCourseGuids(mocLearningPaths);
 				}
