95bc7e531c6ad0920815ca133b13962590b76f7f
Zain Noushad <zain.noushad@quickstart.com>
2019-10-28 13:30:27 +0500

    "LMS subscription automation" For Ticket : 6124

---
+++ LS360ApiGateway/src/main/java/com/softech/ls360/api/gateway/endpoint/restful/SubscriptionKitRestEndPoint.java
@@ -0,0 +1,71 @@
+package com.softech.ls360.api.gateway.endpoint.restful;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.softech.ls360.api.gateway.config.spring.annotation.RestEndpoint;
+import com.softech.ls360.api.gateway.service.SubscriptionKitService;
+import com.softech.ls360.lms.repository.entities.SubscriptionKit;
+
+@RestEndpoint
+@RequestMapping(value = "/")
+public class SubscriptionKitRestEndPoint {
+	
+	@Autowired
+	private SubscriptionKitService subscriptionKitService;
+	
+	
+	@RequestMapping(value="/subscription/kit/add", method = RequestMethod.POST)
+	@ResponseBody
+	public Object addSubscription(@RequestBody Map<Object, Object> request) {
+		Map<Object,Object> responseBody=new HashMap<>();
+		List<Map<Object, Object>> status=new ArrayList<>();
+		try {
+			List<Map<Object, Object>> subscriptionGuid=(List<Map<Object, Object>>) request.get("subscription");
+			for(Map<Object, Object> subscription :  subscriptionGuid) {
+				for(Object subsGuid : subscription.keySet()) {
+					
+					Map<Object,Object> statusOfGuid=new HashMap<>();
+					String paramGuid = (subsGuid == null ? "" : subsGuid.toString());
+					String paramName = (subscription.get(subsGuid) == null ? "" : subscription.get(subsGuid).toString());
+					
+					statusOfGuid.put(paramGuid, paramName);
+					
+					boolean recordExist = subscriptionKitService.findByNameAndGuid(paramName,paramGuid);
+					
+					if(!recordExist){
+						SubscriptionKit kit=new SubscriptionKit();
+						kit.setGuid(paramGuid);
+						kit.setName(paramName);
+						subscriptionKitService.addSubscriptionKit(kit);
+						statusOfGuid.put("status", "added");
+					}
+					else	statusOfGuid.put("status", "already exists");
+					
+					status.add(statusOfGuid);
+					
+				}
+			}
+			
+			
+			responseBody.put("result", status);
+			responseBody.put("message", "success");
+			responseBody.put("status", Boolean.TRUE);
+			
+		} catch (Exception e) {
+			responseBody.put("result", "");
+			responseBody.put("message", e.getMessage());
+			responseBody.put("status", Boolean.FALSE);
+			e.printStackTrace();
+		}
+		return responseBody;
+	}
+}
---
+++ LS360ApiGatewayService/src/main/java/com/softech/ls360/api/gateway/service/impl/SubscriptionKitImpl.java
@@ -0,0 +1,32 @@
+package com.softech.ls360.api.gateway.service.impl;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.softech.ls360.api.gateway.service.SubscriptionKitService;
+import com.softech.ls360.lms.repository.entities.SubscriptionKit;
+import com.softech.ls360.lms.repository.repositories.SubscriptionKitRepository;
+
+@Service
+public class SubscriptionKitImpl implements SubscriptionKitService {
+
+	@Inject
+	private SubscriptionKitRepository subscriptionKitRepository;
+	
+	
+	@Override
+	public boolean findByNameAndGuid(String name, String guid) {
+		List<SubscriptionKit> kit=subscriptionKitRepository.findByNameAndGuid(name, guid);
+		return !kit.isEmpty();
+	}
+
+	@Override
+	public SubscriptionKit addSubscriptionKit(SubscriptionKit subscriptionKit) {
+		SubscriptionKit savedSubscription = subscriptionKitRepository.save(subscriptionKit);
+		return savedSubscription;
+	}
+
+}
