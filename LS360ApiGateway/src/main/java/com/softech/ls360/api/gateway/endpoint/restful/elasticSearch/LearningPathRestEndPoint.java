5b45063cb416598d5ad6dc086b60c6cb14875987
Zain Noushad <zain.noushad@quickstart.com>
2020-01-28 18:07:46 +0500

    NEW TEST

--- LS360ApiGateway/src/main/java/com/softech/ls360/api/gateway/endpoint/restful/elasticSearch/LearningPathRestEndPoint.java
+++ LS360ApiGateway/src/main/java/com/softech/ls360/api/gateway/endpoint/restful/elasticSearch/LearningPathRestEndPoint.java
@@ -783,7 +783,7 @@ public class LearningPathRestEndPoint {
 		
 		try {
 			String username = SecurityContextHolder.getContext().getAuthentication().getName();
-			responseBody.put("result", "CI-CD TEST API");
+			responseBody.put("result", "CI-CD TEST API-CHANGES");
 			responseBody.put("message", username);
 			responseBody.put("status", Boolean.FALSE);
 			
